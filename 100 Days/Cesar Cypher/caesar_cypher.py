a52926ec9cf8a0df974a76b2c6bb8116de7d4b22
Oscar Ramirez <36704574+o-ramirez@users.noreply.github.com>
2021-08-15 12:01:36 -0400

    Update caesar_cypher.py

--- 100 Days/Cesar Cypher/caesar_cypher.py
+++ 100 Days/Cesar Cypher/caesar_cypher.py
@@ -40,18 +40,6 @@ def caesar(text, shift, direction):
     print(f'The decoded text is {cipher_text.join(cipher_list)}') 
 
 
-#caesar(text = text, shift = shift, direction = direction)
-
-
-
-
-    #TODO-3: What happens if the user enters a number/symbol/space?
-    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-    #e.g. start_text = "meet me at 3"
-    #end_text = "•••• •• •• 3"
-    
-
-
 
 #TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
 #e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
